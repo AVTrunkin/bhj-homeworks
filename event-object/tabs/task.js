a58e0858b0b75d01107dbcf72344ab9f46ca79a2
Aleksander <av.trunkin@gmail.com>
2026-05-28 08:24:00 +0300

    first commit tabs

---
+++ event-object/tabs/task.js
@@ -0,0 +1,16 @@
+document.querySelectorAll('.tab__navigation').forEach(tabNavigation => {
+  const tabs = Array.from(tabNavigation.querySelectorAll('.tab'));
+  const tabContents = Array.from(
+    tabNavigation.nextElementSibling.querySelectorAll('.tab__content')
+  );
+
+  tabs.forEach((tab, index) => {
+    tab.addEventListener('click', () => {
+      tabs.forEach(t => t.classList.remove('tab_active'));
+      tabContents.forEach(c => c.classList.remove('tab__content_active'));
+
+      tab.classList.add('tab_active');
+      tabContents[index].classList.add('tab__content_active');
+    });
+  });
+});
